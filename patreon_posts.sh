09e9a9679a6f3c146fe475f0ac6f10955d066669
Robert <misternumberone@live.com>
2023-07-15 11:32:21 -0500

    update token scraper xpaths and adjust wrapper script to print to stdout

--- patreon_posts.sh
+++ patreon_posts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export DISPLAY=:0.0
 cd ~/patreon-index
-./update_patreon_token.py > update_patreon_token.log 2>&1
+./update_patreon_token.py 2>&1 | tee update_patreon_token.log
 export PATREON_ACCESS_TOKEN=$(cat patreon_token.txt)
-./patreon_posts.py --download-posts --download-media --process-media --generate-site > patreon_posts.log 2>&1
+./patreon_posts.py --download-posts --download-media --process-media --generate-site 2>&1 | tee patreon_posts.log
 cp patreon.html.j2 patreon.html
 cp -- *.html /var/www/html
 cp -- *.css  /var/www/html
